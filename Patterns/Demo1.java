7ad1693c7dcacc2e940faf7da873fe38f3cd2430
niyatipatil <patilniyati106@gmail.com>
2024-04-19 00:13:15 +0530

    Added new Files

---
+++ Patterns/Demo1.java
@@ -0,0 +1,19 @@
+
+//Printing hello world
+//Printing a block of stars by user input; time complexity = O(n^2)
+import java.util.Scanner;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("Enter the Square side value : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+}
--- Patterns/RightAngledTriangle.java
+++ Patterns/RightAngledTriangle.java
@@ -8,8 +8,8 @@ public class RightAngledTriangle {
         System.out.println("Enter the number:");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println("");
